117395359d55e71ba307d7ca491308c3bbdbd8d4
Tomasz Wylezek <tomwylezek@gmail.com>
2020-03-20 18:17:46 +0100

    Add tests for Search

---
+++ src/DataTable/Search/Search.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { Search, ISearchProps } from './Search';
+
+describe('ISearchProps', () => {
+  let props: ISearchProps;
+
+  beforeEach(() => {
+    props = {
+      value: '',
+      handleChange: jest.fn()
+    };
+  });
+
+  it('renders without crashing', () => {
+    const component = shallow(<Search {...props} />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('calls a function when button clicked', () => {
+    const component = mount(<Search {...props} />);
+
+    component.find('input').simulate('change');
+
+    expect(props.handleChange).toHaveBeenCalled();
+  });
+});
